9acbc6b23f91e8656089a94b3afdf04afcc327a2
= <pinildissanayaka@gmail.com>
2024-08-05 19:25:50 +0530

    add battery level indicator

--- HIDBleKeboard/HIDBleKeboard.ino
+++ HIDBleKeboard/HIDBleKeboard.ino
@@ -1,7 +1,8 @@
 #include <BleKeyboard.h>
 #include <Keypad.h>
+#include <Pangodream_18650_CL.h>
 
-BleKeyboard bleKeyboard("Macropad");
+int batteryLevel = 100;
 
 const byte ROWS = 4; 
 const byte COLS = 4; 
@@ -16,13 +17,17 @@ char keys[ROWS][COLS] = {
 byte rowPins[ROWS] = {9, 8, 7, 6}; 
 byte colPins[COLS] = {5, 4, 3, 2}; 
 
-Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+Keypad customKeypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS); 
+BleKeyboard bleKeyboard("Macropad", "Espressif", batteryLevel);
+Pangodream_18650_CL BL;
+//Pangodream_18650_CL BL(ADC_PIN, CONV_FACTOR, READS);
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting BLE work!");
   bleKeyboard.begin();
 }
+
 
 void loop() {
 
@@ -35,3 +40,18 @@ void loop() {
     
   }
 }
+
+void getBatteryLevel(){
+  Serial.print("Value from pin: ");
+  Serial.println(analogRead(34));
+  Serial.print("Average value from pin: ");
+  Serial.println(BL.pinRead());
+  Serial.print("Volts: ");
+  Serial.println(BL.getBatteryVolts());
+  Serial.print("Charge level: ");
+  batteryLevel = BL.getBatteryChargeLevel();
+  Serial.println(batteryLevel);
+  Serial.println("");
+  delay(1000);
+}
+
