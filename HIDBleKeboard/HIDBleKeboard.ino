7893fd25344a2210d596b078a31b30bd77a711cb
= <pinildissanayaka@gmail.com>
2024-08-05 17:25:18 +0530

    add keypad

--- HIDBleKeboard/HIDBleKeboard.ino
+++ HIDBleKeboard/HIDBleKeboard.ino
@@ -1,6 +1,22 @@
 #include <BleKeyboard.h>
-//Set the name of the bluetooth keyboard (that shows up in the bluetooth menu of your device)
-BleKeyboard bleKeyboard("ESP_KEYBOARD");
+#include <Keypad.h>
+
+BleKeyboard bleKeyboard("Macropad");
+
+const byte ROWS = 4; 
+const byte COLS = 4; 
+
+char keys[ROWS][COLS] = {
+  {'a', 'b', 'c', 'd'},
+  {'e', 'f', 'g', 'h'},
+  {'i', 'k', 'l', 'm'},
+  {'n', 'o', 'p', 'q'}
+};
+
+byte rowPins[ROWS] = {9, 8, 7, 6}; 
+byte colPins[COLS] = {5, 4, 3, 2}; 
+
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
 
 void setup() {
   Serial.begin(115200);
@@ -11,10 +27,11 @@ void setup() {
 void loop() {
 
   if (bleKeyboard.isConnected()) {
+    char customKey = customKeypad.getKey();
+
     bleKeyboard.write(KEY_MEDIA_VOLUME_DOWN);
     delay(2000);
 
     
   }
 }
-
